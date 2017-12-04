55ac6fd098edc8aa5194380c4ca7e52bea020d69
Kanav07 <kanavbansal07@gmail.com>
2017-12-04 20:00:25 +0530

    adding kmp algorithm frm string search

--- src/Questions/SubString.java
+++ src/Questions/SubString.java
@@ -34,11 +34,11 @@ public class SubString {
         /*String haystack = "kansjsjsjjsav";
         String needle = "kanav";*/
 //        String haystack = "bbaabbbbbaabbaabbbbbbabbbabaabbbabbabbbbababbbabbabaaababbbaabaaaba";
-//        String needle = "babaaa";
+//        String needle = "aabaabaaa";
         String haystack = "aabaaaababaabbbabbabbbaabababaaaaaababaaabbabbabbabbaaaabbbbbbaabbabbbbbabababbaaabbaabbbababbb";
         String needle = "bba";
-//        String input = "cccb";
-        //String input = "";
+//        String haystack = "baabaaabbbababbbbbbaababbbaabbbbaabababababbaa";
+//        String needle = "babbabaabababbbababaababbaabbbaaaabaaabbab";
 
         /*int inputListSize = sc.nextInt();
         int inputNumber;
@@ -49,39 +49,99 @@ public class SubString {
         }*/
         long start = System.nanoTime();
         //call function here.....
-        System.out.println(strStr(haystack,needle));
+        System.out.println(strStr(haystack, needle));
+
         long end = System.nanoTime();
+
+        System.out.println("\nTime taken : " + (end - start));
+
+        start = System.nanoTime();
+        //call function here.....
+        System.out.println(strStrKMPAlgorithm(haystack, needle));
+        end = System.nanoTime();
 
         System.out.println("\nTime taken : " + (end - start));
 
     }
 
 
-
-    public static  int strStr(final String haystack, final String needle) {
+    public static int strStr(final String haystack, final String needle) {
         int haystackSize = haystack.length(), needleSize = needle.length();
-        if ( needleSize == 0 || haystackSize==0) return 0;
-        if ( haystackSize < needleSize) return -1;
+        if (needleSize == 0 || haystackSize == 0) return 0;
+        if (haystackSize < needleSize) return -1;
 
-        int haystackCursor=0, needleCursor=0;
+        int haystackCursor = 0, needleCursor = 0;
 
-        while (haystackCursor < haystackSize && needleCursor < needleSize){
-            if ( needle.charAt(needleCursor) == haystack.charAt(haystackCursor)) {
+        while (haystackCursor < haystackSize && needleCursor < needleSize) {
+            if (needle.charAt(needleCursor) == haystack.charAt(haystackCursor)) {
                 needleCursor++;
                 haystackCursor++;
-            }else {
-                if ( needleCursor < needleSize )  needleCursor=0;
-                if ( needle.charAt(needleCursor) == haystack.charAt(haystackCursor)){
+            } else {
+                if (needleCursor > 0 && needleCursor < needleSize) {
+                    haystackCursor = haystackCursor - needleCursor + 1;
+                    needleCursor = 0;
+                }
+                if (haystackCursor < haystackSize && needle.charAt(needleCursor) == haystack.charAt(haystackCursor)) {
                     needleCursor++;
                     haystackCursor++;
-                }else
+                } else
                     haystackCursor++;
             }
         }
-        if ( needleCursor == needleSize) return haystackCursor - needleCursor;
-        else if ( haystackCursor == haystackSize && needleCursor < needleSize) return -1;
+        if (needleCursor == needleSize) return haystackCursor - needleCursor;
+        else if (haystackCursor == haystackSize && needleCursor < needleSize) return -1;
 
-        return 2;
+        return -1;
+    }
+
+//https://www.youtube.com/watch?v=GTJr8OvyEVQ
+
+    public static int strStrKMPAlgorithm(final String haystack, final String needle) {
+
+        int haystackSize = haystack.length(), needleSize = needle.length();
+        if (needleSize == 0 || haystackSize == 0) return 0;
+        if (haystackSize < needleSize) return -1;
+
+        //preparing the LPS array
+        int[] LPS = new int[needleSize];
+        LPS[0] = 0;
+        for (int i = 0, j = 1; j < needleSize && i < needleSize; ) {
+            if (needle.charAt(i) != needle.charAt(j)) {
+                if (i > 0) {
+                    while (i > 0) {
+                        if (needle.charAt(i) == needle.charAt(j)) {
+                            LPS[j] = LPS[i] + 1;
+                            i++;
+                            j++;
+                            break;
+                        } else {
+                            i = LPS[i - 1];
+                        }
+                    }
+                } else {
+                    LPS[j] = 0;
+                    j++;
+                }
+            } else {
+                LPS[j] = i + 1;
+                i++;
+                j++;
+            }
+        }
+
+        for (int i = 0, j = 0; i < haystackSize && j < needleSize; ) {
+            if (needle.charAt(j) == haystack.charAt(i)) {
+                i++;
+                j++;
+                if (j == needleSize) return i - j;
+            } else {
+                if (j > 0) j = LPS[j - 1];
+                else i++;
+            }
+        }
+        return -1;
+
+
     }
 }
 
